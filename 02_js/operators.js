1bd807066b6abf436c0c068ec309c6fbb746866c
Srijani Seth <sethsrijani@gmail.com>
2026-02-23 22:40:23 +0530

    Save local changes

---
+++ 02_js/operators.js
@@ -0,0 +1,14 @@
+
+let a = 10 , b = 3;
+
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+let mod = a % b;    
+
+console.log("Sum : " + sum);
+console.log("Sub : " + sub);
+console.log("Mul : " + mul);
+console.log("Div : " + div);
+console.log("Mod : " + mod);
---
+++ CodingChallenge/21_02_26/Grade_calculator.js
@@ -0,0 +1,15 @@
+let marks = 100;
+
+if(marks>= 90 && marks<=100) {
+    console.log("you have got grade A")
+} else if(marks>=80 && marks<=89) {
+    console.log("you have got grade B")
+} else if(marks>=70 && marks<=79) {
+    console.log("you have got grade C")
+}else if(marks>=60 && marks<=69) {
+    console.log("you have got grade D")
+}else if(marks>=0 && marks<=59){
+    console.log("you have grade F")  
+}else{
+   console.log("undefined")
+}
